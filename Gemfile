dd69f0f6b6a50416b0744e654126ef88b0f64cc9
Italo Matos <italomatos@gmail.com>
2020-03-19 20:39:15 -0300

    added test to return

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :development, :test do
   gem 'rspec-rails'
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '~> 1.4'
+  gem 'rspec-json_expectations'
 end
 
 group :development do
--- app/controllers/api/v1/corona_controller.rb
+++ app/controllers/api/v1/corona_controller.rb
@@ -2,41 +2,30 @@ class Api::V1::CoronaController < ApplicationController
 
   def find_by_date
 
-  date = (params[:date] ? params[:date] : (Time.now - 1.day))
+    date = (params[:date] ? params[:date] : (Time.now - 1.day))
 
-	begin
+    begin
+      date = date.to_date.strftime('%d/%m/%Y')
+      result = { date: date, data: [] }
+      country = params[:country]
+      data = corona_data.find{ |item| item['date'] == date }
 
-    date = date.to_date.strftime('%d/%m/%Y')
-    result = {date: date, data: []}
-
-
-
-
-    country = params[:country]
-    data = corona_data.find{ |item| item['date'] == date }
-
-    if data and data.key? 'values'
-    data['values'].each do |item|
-      if country
-        result[:data] << item if item['uid'] == country
-      else
-        result[:data] << item
+      if data and data.key? 'values'
+      data['values'].each do |item|
+        if country
+          result[:data] << item if item['uid'] == country
+        else
+          result[:data] << item
+        end
       end
+      else
+        result[:date] = date
+      end
+
+      render json: result
+    rescue ArgumentError
+      render json: { status: 'not_found' }, status: :not_found
     end
-    else
-      result[:date] = date
-    end
-
-
-    # result = result['values'].find{|item| item['uid'].eql? (params[:country])}
-
-    render json: result
-	rescue ArgumentError
-    render json: { status: 'not_found' }, status: :not_found
-	end
-
-
-
   end
 
   #all
