9ac8c6d95570ffde91a1878c0b22cf48870466b9
Italo Matos <italomatos@gmail.com>
2020-03-19 19:16:41 -0300

    fixed endpoint

--- app/controllers/api/v1/corona_controller.rb
+++ app/controllers/api/v1/corona_controller.rb
@@ -5,13 +5,32 @@ class Api::V1::CoronaController < ApplicationController
   date = (params[:date] ? params[:date] : (Time.now - 1.day))
 
 	begin
-    date = date.to_date.strftime('%d/%m/%Y')
-    country = params[:country]
-    result = corona_data.find{ |item| item['date'] == date }
-    result = {} if result.nil?
-    result = result['values'].find{|item| item['uid'].eql? (params[:country])} if country
 
-    render json: result.merge({date: date})
+    date = date.to_date.strftime('%d/%m/%Y')
+    result = {date: date, data: []}
+
+
+
+
+    country = params[:country]
+    data = corona_data.find{ |item| item['date'] == date }
+
+    if data and data.key? 'values'
+    data['values'].each do |item|
+      if country
+        result[:data] << item if item['uid'] == country
+      else
+        result[:data] << item
+      end
+    end
+    else
+      result[:date] = date
+    end
+
+
+    # result = result['values'].find{|item| item['uid'].eql? (params[:country])}
+
+    render json: result
 	rescue ArgumentError
     render json: { status: 'not_found' }, status: :not_found
 	end
