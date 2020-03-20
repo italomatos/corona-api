9c8ba76f645b4e815214b2e23039a947322e4e58
Italo Matos <italomatos@gmail.com>
2020-03-19 21:21:20 -0300

    added rubocop

--- app/controllers/api/v1/corona_controller.rb
+++ app/controllers/api/v1/corona_controller.rb
@@ -1,23 +1,23 @@
+# frozen_string_literal: true
+
 class Api::V1::CoronaController < ApplicationController
-
   def find_by_date
-
-    date = (params[:date] ? params[:date] : (Time.now - 1.day))
+    date = (params[:date] || (Time.now - 1.day))
 
     begin
       date = date.to_date.strftime('%d/%m/%Y')
       result = { date: date, data: [] }
       country = params[:country]
-      data = corona_data.find{ |item| item['date'] == date }
+      data = corona_data.find { |item| item['date'] == date }
 
-      if data and data.key? 'values'
-      data['values'].each do |item|
-        if country
-          result[:data] << item if item['uid'] == country
-        else
-          result[:data] << item
+      if data&.key?('values')
+        data['values'].each do |item|
+          if country
+            result[:data] << item if item['uid'] == country
+          else
+            result[:data] << item
+          end
         end
-      end
       else
         result[:date] = date
       end
@@ -28,14 +28,15 @@ class Api::V1::CoronaController < ApplicationController
     end
   end
 
-  #all
+  # all
   def index
-  	render json: corona_data
+    render json: corona_data
   end
 
   protected
+
   def corona_data
-  	response = HTTParty.get('http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js')
-  	JSON.parse(response.body.gsub('var database=', ''))['world']
+    response = HTTParty.get('http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js')
+    JSON.parse(response.body.gsub('var database=', ''))['world']
   end
 end
--- spec/requests/api/v1/coronas_spec.rb
+++ spec/requests/api/v1/coronas_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Coronas', type: :request do
@@ -8,40 +10,40 @@ RSpec.describe 'Api::V1::Coronas', type: :request do
     end
 
     it 'check return data' do
-    	get api_v1_corona_path
+      get api_v1_corona_path
       expect(response.body).to include_json(
-        [{ date: /\d+\/\d+\/\d+/ } ]
+        [{ date: %r{\d+/\d+/\d+} }]
       )
     end
   end
 
   describe 'GET /api/v1/corona/:date' do
-  	it '200 OK' do
-  		get api_v1_corona_by_date_path(date: Time.now - 1.day)
-  		expect(response).to have_http_status(200)
-  	end
+    it '200 OK' do
+      get api_v1_corona_by_date_path(date: Time.now - 1.day)
+      expect(response).to have_http_status(200)
+    end
 
-  	it 'request with invalid date' do
+    it 'request with invalid date' do
       get api_v1_corona_by_date_path(date: '2020-02-31')
       expect(response).to have_http_status(404)
-  	end
+    end
 
-  	it 'request with valid date, but no data' do
+    it 'request with valid date, but no data' do
       date = Time.now + 5.day
-  	  get api_v1_corona_by_date_path(date: date)
-  	  expect(response.body).to include_json(
+      get api_v1_corona_by_date_path(date: date)
+      expect(response.body).to include_json(
         date: date.strftime('%d/%m/%Y'),
         data: []
       )
-  	end
+    end
 
-  	it 'request with valid date and valid country - BR' do
+    it 'request with valid date and valid country - BR' do
       date = Time.now - 5.day
       get api_v1_corona_by_date_path(date: date), params: { country: 'BR' }
       expect(response.body).to include_json(
         date: date.strftime('%d/%m/%Y'),
         data: [{ uid: 'BR' }]
       )
-  	end
+    end
   end
 end
