2768ea35996cccd4411256b4ffab4c539703a170
Italo Matos <italomatos@gmail.com>
2020-03-19 21:40:01 -0300

    improve Corona Controller

--- app/controllers/api/v1/corona_controller.rb
+++ app/controllers/api/v1/corona_controller.rb
@@ -1,42 +1,46 @@
 # frozen_string_literal: true
 
-class Api::V1::CoronaController < ApplicationController
-  def find_by_date
-    date = (params[:date] || (Time.now - 1.day))
+module Api
+  module V1
+    class CoronaController < ApplicationController
+      def find_by_date
+        date = (params[:date] || (Time.now - 1.day))
 
-    begin
-      date = date.to_date.strftime('%d/%m/%Y')
-      result = { date: date, data: [] }
-      country = params[:country]
-      data = corona_data.find { |item| item['date'] == date }
+        begin
+          date = date.to_date.strftime('%d/%m/%Y')
+          result = { date: date, data: [] }
+          country = params[:country]
+          data = corona_data.find { |item| item['date'] == date }
 
-      if data&.key?('values')
-        data['values'].each do |item|
-          if country
-            result[:data] << item if item['uid'] == country
+          if data&.key?('values')
+            data['values'].each do |item|
+              if country
+                result[:data] << item if item['uid'] == country
+              else
+                result[:data] << item
+              end
+            end
           else
-            result[:data] << item
+            result[:date] = date
           end
+
+          render json: result
+        rescue ArgumentError
+          render json: { status: 'not_found' }, status: :not_found
         end
-      else
-        result[:date] = date
       end
 
-      render json: result
-    rescue ArgumentError
-      render json: { status: 'not_found' }, status: :not_found
+      # all
+      def index
+        render json: corona_data
+      end
+
+      protected
+
+      def corona_data
+        response = HTTParty.get('http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js')
+        JSON.parse(response.body.gsub('var database=', ''))['world']
+      end
     end
-  end
-
-  # all
-  def index
-    render json: corona_data
-  end
-
-  protected
-
-  def corona_data
-    response = HTTParty.get('http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js')
-    JSON.parse(response.body.gsub('var database=', ''))['world']
   end
 end
